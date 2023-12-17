8e6c438e7e784a007bd9b08965e3fe711e7472ca
Valentin_cabrel22 <cabreltaya@gmail.com>
2023-12-17 11:42:31 +0100

    modify bd infos

--- wp-config.php
+++ wp-config.php
@@ -21,13 +21,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'restaurant');
+define('DB_NAME', 'chekscooking');
 
 /** Database username */
-define('DB_USER', 'root');
+define('DB_USER', 'chekscooking');
 
 /** Database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'wR9#x2$Lz!vF');
 
 /** Database hostname */
 define('DB_HOST', 'localhost');
